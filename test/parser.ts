a5917b48b0d559b9555a58adaee5ca587ec2b84d
Lu Zhu <lu.zhu@instacart.com>
2020-06-27 19:23:11 -0700

    Add parseBestMove test

--- test/parser.ts
+++ test/parser.ts
@@ -3,6 +3,18 @@ import { expect } from "chai";
 import { Parser } from "../src";
 
 describe("Parser", (): void => {
+    describe("parseBestMove", () => {
+        it("move only", (): void => {
+            const result = Parser.parseBestMove('bestmove e2e4');
+            expect(result).to.deep.eq(['e2e4', null]);
+        });
+
+        it("move and ponder", (): void => {
+            const result = Parser.parseBestMove('bestmove e2e4 ponder e7e5');
+            expect(result).to.deep.eq(['e2e4', 'e7e5']);
+        });
+    });
+
     describe("parseOption", (): void => {
         it("no default", (): void => {
             const result = Parser.parseOption('option name Debug Log File type string default');
